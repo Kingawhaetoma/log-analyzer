8553899aa9662c046bd5c187da306d8cfd53445f
King Awhaetoma <king.awhaetoma@bsu.edu>
2026-02-16 02:14:09 -0500

    Fix CI import path

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -1,3 +1,7 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from log_analyzer import parse_events, analyze
 
 def test_parse_linux_events():
